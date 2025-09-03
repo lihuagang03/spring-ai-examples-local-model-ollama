fb008b0967192b3f42a6bc02ea7b7c3c6ca396db
lihuagang <bert825_work@163.com>
2025-09-03 21:02:19 +0800

    spring.ai.mcp.server.protocol=STATELESS|STREAMABLE

--- model-context-protocol/weather/manual-webflux-server/src/main/java/org/springframework/ai/mcp/sample/server/McpServerConfig.java
+++ model-context-protocol/weather/manual-webflux-server/src/main/java/org/springframework/ai/mcp/sample/server/McpServerConfig.java
@@ -1,7 +1,9 @@
 package org.springframework.ai.mcp.sample.server;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.modelcontextprotocol.server.McpServer;
+import io.modelcontextprotocol.server.McpServerFeatures;
 import io.modelcontextprotocol.server.McpSyncServer;
 import io.modelcontextprotocol.server.transport.StdioServerTransportProvider;
 import io.modelcontextprotocol.server.transport.WebFluxSseServerTransportProvider;
@@ -10,13 +12,18 @@ import io.modelcontextprotocol.spec.McpServerTransportProvider;
 
 import org.springframework.ai.mcp.McpToolUtils;
 import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.definition.ToolDefinition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
- * MCP 服务端配置
+ * MCP 服务器配置
  */
 @Configuration
 public class McpServerConfig {
@@ -25,7 +32,7 @@ public class McpServerConfig {
 	@Bean
 	@ConditionalOnProperty(prefix = "transport", name = "mode", havingValue = "stdio")
 	public StdioServerTransportProvider stdioServerTransportProvider() {
-		// 标准IO 服务端传输通信提供者
+		// 标准IO 服务器传输通信提供者
 		return new StdioServerTransportProvider();
 	}
 
@@ -33,7 +40,7 @@ public class McpServerConfig {
 	@Bean
 	@ConditionalOnProperty(prefix = "transport", name = "mode", havingValue = "sse")
 	public WebFluxSseServerTransportProvider sseServerTransportProvider() {
-		// 流式SSE 服务端传输通信提供者
+		// 流式SSE 服务器传输通信提供者
 		return new WebFluxSseServerTransportProvider(new ObjectMapper(), "/mcp/message");
 	}
 
@@ -52,23 +59,53 @@ public class McpServerConfig {
 		return new WeatherApiClient();
 	}
 
+	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
 	@Bean
 	public McpSyncServer mcpServer(McpServerTransportProvider transportProvider, WeatherApiClient weatherApiClient) { // @formatter:off
 
 		// Configure server capabilities with resource support
-		// 服务端功能
+		// 服务器功能
 		var capabilities = McpSchema.ServerCapabilities.builder()
 			.tools(true) // Tool support with list changes notifications
 			.logging() // Logging support
 			.build();
 
+		// 工具列表
+		ToolCallback[] toolCallbacks = ToolCallbacks.from(weatherApiClient);
+		System.out.println("\ntoolCallbacks:");
+		Arrays.stream(toolCallbacks).forEach(toolCallback -> {
+			ToolDefinition toolDefinition = toolCallback.getToolDefinition();
+			try {
+				String jsonString = OBJECT_MAPPER.writeValueAsString(toolDefinition);
+				System.out.println(jsonString);
+				System.out.println();
+			} catch (JsonProcessingException e) {
+//				e.printStackTrace();
+			}
+		});
+
+		List<McpServerFeatures.SyncToolSpecification> toolSpecifications =
+				McpToolUtils.toSyncToolSpecifications(toolCallbacks);
+		System.out.println("\ntoolSpecifications:");
+		toolSpecifications.forEach(toolSpecification -> {
+			McpSchema.Tool tool = toolSpecification.tool();
+			try {
+				String jsonString = OBJECT_MAPPER.writeValueAsString(tool);
+				System.out.println(jsonString);
+				System.out.println();
+			} catch (JsonProcessingException e) {
+//				e.printStackTrace();
+			}
+		});
+
 		// Create the server with both tool and resource capabilities
-		// MCP 同步服务端
+		// MCP服务器
 		McpSyncServer server = McpServer.sync(transportProvider)
-			.serverInfo("MCP Demo Weather Server", "1.0.0") // 服务端信息
-			.capabilities(capabilities)
-			.tools(McpToolUtils.toSyncToolSpecifications(ToolCallbacks.from(weatherApiClient))) // Add @Tools
-			.build();
+				.serverInfo("MCP Demo Weather Server", "1.0.0") // 服务器信息
+				.capabilities(capabilities)
+				.tools(toolSpecifications) // Add @Tools
+				.build();
 		
 		return server; // @formatter:on
 	} // @formatter:on
